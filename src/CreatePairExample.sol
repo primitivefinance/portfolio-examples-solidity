4e3519e1d20c56b6cd0404b36ef2f1b6fefdccdf
clemlak <clementlakhal@gmail.com>
2023-05-12 17:33:17 +0400

    feat: update createPair example

--- src/CreatePairExample.sol
+++ src/CreatePairExample.sol
@@ -11,6 +11,9 @@ contract CreatePairExample {
     }
 
     function createPair(address asset, address quote) external {
+        // Creating a new pair is extremely simple and only requires to pass the
+        // address of two different tokens. Keep in mind that the decimals must
+        // be between 6 and 18.
         portfolio.createPair(asset, quote);
     }
 }
