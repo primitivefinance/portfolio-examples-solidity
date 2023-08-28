790b58979d63ab29ed4993dd3283e4a60ae8b383
clemlak <clementlakhal@gmail.com>
2023-08-28 11:55:30 +0400

    feat: update deallocate example

--- src/DeallocateExample.sol
+++ src/DeallocateExample.sol
@@ -42,18 +42,20 @@ contract DeallocateExample is ERC1155TokenReceiver {
             false, address(this), poolId, maxLiquidity, deltaAsset, deltaQuote
         );
 
-        // Since we were the first liquidity provider, a part of our liquidity
-        // was burnt, we need to remove that from our calculations:
-        uint128 liquidity = maxLiquidity - uint128(1e9);
+        // We can use the `balanceOf` function to check how much liquidity we
+        // currently own.
+        uint256 balance =
+            ERC1155(address(portfolio)).balanceOf(address(this), poolId);
 
         // To avoid being sandwiched, we can check the minimum amount of
         // tokens that we can expect from our liquidity balance.
+        // Note: Don't forget the "-" sign since we're deallocating!
         (uint128 minDeltaAsset, uint128 minDeltaQuote) =
-            portfolio.getLiquidityDeltas(poolId, int128(liquidity));
+            portfolio.getLiquidityDeltas(poolId, -int128(uint128(balance)));
 
-        // We can now deallocate the liquidity we just allocated.
-        // Note that we have to remove the burnt liquidity (1e9), since we were
-        // the first to provide liquidity in this pool.
-        portfolio.deallocate(false, poolId, liquidity, 0, 0);
+        // We can now deallocate our liquidity:
+        portfolio.deallocate(
+            false, poolId, uint128(balance), minDeltaAsset, minDeltaQuote
+        );
     }
 }
